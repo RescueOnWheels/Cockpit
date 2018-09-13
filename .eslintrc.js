57641de8f92697c4f29599fd33ae65bcaadb06bd
Martijn Vegter <mail@mvegter.me>
2018-09-13 12:18:32 +0200

    Corrected mistakes
    Extend ESLint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "rules": {
+        "multiline-comment-style": ["error", "starred-block"],
+        "no-console": ["error", {
+            "allow": ["error", "warn"]
+        }],
+        "no-multiple-empty-lines": ["error", {
+            "max": 1,
+            "maxBOF": 0,
+            "maxEOF": 1
+        }],
+        "object-property-newline": ["error", {
+            "allowAllPropertiesOnSameLine": false
+        }],
+    }
 };
