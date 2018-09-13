5c11815dbcaafc510fff756565e237b7b2566c21
Martijn Vegter <martijn@martijnvegter.com>
2018-09-13 12:07:36 +0200

    It compiles! Ship it!
    Added ESLint and Husky.

--- index.js
+++ index.js
@@ -1,93 +1,95 @@
-var SteamController = require('./Controller');
-var controller = new SteamController();
+const io = require('socket.io')();
+const SteamController = require('./Controller');
 
-var speed = 0;
-var direction = 1;
-var balance = 0;
+const controller = new SteamController();
 
-var loopTurnLeft, loopTurnRight;
-const loopInterval = 1;
+let speed = 0;
+let direction = 1;
+let balance = 0;
 
-controller.lshoulder.on('press', () => {
-    loopTurnLeft = setInterval(turnLeft, loopInterval);
-});
-
-controller.lshoulder.on('release', () => {
-    clearInterval(loopTurnLeft);
-    loopTurnLeft = undefined;
-});
-
-controller.rshoulder.on('press', () => {
-    loopTurnRight = setInterval(turnRight, loopInterval);
-});
-
-controller.rshoulder.on('release', () => {
-    clearInterval(loopTurnRight);
-    loopTurnRight = undefined;
-});
-
-controller.ltrigger.on('move', (event) => {
-    if (event.delta == 0) {
-        return;
-    }
-
-    var value = event.normval;
-    value *= -100;
-    value = Math.round(value);
-
-    direction = -1;
-    speed = value;
-});
-
-controller.rtrigger.on('move', (event) => {
-    if (event.delta == 0) {
-        return;
-    }
-
-    var value = event.normval;
-    value *= 100;
-    value = Math.round(value);
-
-    direction = 1;
-    speed = value;
-});
-
-controller.stick.on('move', (event) => {
-    if (event.delta == 0) {
-        return;
-    }
-
-    var value = event.normx;
-    value *= 100;
-    value = Math.round(value);
-
-    balance = value;
-});
-
-controller.y.on('press', () => {
-    speed = 0;
-    direction = 1;
-    balance = 0;
-});
-
-var io = require('socket.io')();
-io.listen(3000);
+let loopTurnLeft;
+let loopTurnRight;
 
 function turnLeft() {
-    io.sockets.emit('move', 'left');
+  io.sockets.emit('move', 'left');
 }
 
 function turnRight() {
-    io.sockets.emit('move', 'right');
+  io.sockets.emit('move', 'right');
 }
+
 
 function emit() {
-    if (loopTurnLeft != undefined || loopTurnRight != undefined) {
-        return;
-    }
+  if (loopTurnLeft !== undefined || loopTurnRight !== undefined) {
+    return;
+  }
 
-    io.sockets.emit('move', { speed, direction, balance });
+  io.sockets.emit('move', { speed, direction, balance });
 }
 
+controller.lshoulder.on('press', () => {
+  loopTurnLeft = setInterval(turnLeft, 1);
+});
+
+controller.lshoulder.on('release', () => {
+  clearInterval(loopTurnLeft);
+  loopTurnLeft = undefined;
+});
+
+controller.rshoulder.on('press', () => {
+  loopTurnRight = setInterval(turnRight, 1);
+});
+
+controller.rshoulder.on('release', () => {
+  clearInterval(loopTurnRight);
+  loopTurnRight = undefined;
+});
+
+controller.ltrigger.on('move', (event) => {
+  if (event.delta === 0) {
+    return;
+  }
+
+  let value = event.normval;
+  value *= -100;
+  value = Math.round(value);
+
+  direction = -1;
+  speed = value;
+});
+
+controller.rtrigger.on('move', (event) => {
+  if (event.delta === 0) {
+    return;
+  }
+
+  let value = event.normval;
+  value *= 100;
+  value = Math.round(value);
+
+  direction = 1;
+  speed = value;
+});
+
+controller.stick.on('move', (event) => {
+  if (event.delta === 0) {
+    return;
+  }
+
+  let value = event.normx;
+  value *= 100;
+  value = Math.round(value);
+
+  balance = value;
+});
+
+controller.y.on('press', () => {
+  speed = 0;
+  direction = 1;
+  balance = 0;
+});
+
+
+io.listen(3000);
 controller.connect();
 setInterval(emit, 1);
